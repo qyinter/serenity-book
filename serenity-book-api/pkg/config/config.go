ae08305eb268212155b0457b728c23a3ca08676e
江西小徐 <10062942+jiangxi-xiaoxu@user.noreply.gitee.com>
2024-04-21 15:54:01 +0800

    增加全局 G_Config

--- serenity-book-api/pkg/config/config.go
+++ serenity-book-api/pkg/config/config.go
@@ -3,8 +3,10 @@ package config
 import (
 	"flag"
 	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 	"os"
+	"serenity-book-api/internal/global"
 )
 
 func NewConfig() *viper.Viper {
@@ -25,6 +27,19 @@ func getConfig(path string) *viper.Viper {
 	conf.SetConfigFile(path)
 	err := conf.ReadInConfig()
 	if err != nil {
+		panic(err)
+	}
+	// 监控配置文件变化
+	conf.WatchConfig()
+	// 热加载配置文件
+	conf.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("config file changed:", e.Name)
+		if err = conf.Unmarshal(&global.G_CONFIG); err != nil {
+			fmt.Println(err)
+		}
+	})
+	// 初始化配置
+	if err = conf.Unmarshal(&global.G_CONFIG); err != nil {
 		panic(err)
 	}
 	return conf
